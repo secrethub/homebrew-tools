c4981e1a3a263c313ea18e8cb567613de267df1d
goreleaserbot <goreleaser@carlosbecker.com>
2019-04-11 08:41:20 +0200

    Brew formula update for secrethub version v0.21.2

--- Formula/secrethub-cli.rb
+++ Formula/secrethub-cli.rb
@@ -2,9 +2,9 @@
 class SecrethubCli < Formula
   desc "Command-line interface for SecretHub"
   homepage "https://secrethub.io"
-  url "https://github.com/secrethub/secrethub-cli/releases/download/v0.21.1/secrethub-v0.21.1-darwin-amd64.tar.gz"
-  version "0.21.1"
-  sha256 "d16d4056900ba61109dd4fedb577d28249012e2e07cbf52c16c490c3e5f970e0"
+  url "https://github.com/secrethub/secrethub-cli/releases/download/v0.21.2/secrethub-v0.21.2-darwin-amd64.tar.gz"
+  version "0.21.2"
+  sha256 "e4d53e14b202233873e40539125d36bd5862bd5054f4e468ade68877209cfc1e"
 
   def install
     bin.install "bin/secrethub"
