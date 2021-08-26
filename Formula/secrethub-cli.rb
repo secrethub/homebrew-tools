1145240d56070b0987abaac168e0790b6070909f
goreleaserbot <goreleaser@carlosbecker.com>
2021-08-26 20:54:10 +0200

    Brew formula update for secrethub-cli version v0.43.0

--- Formula/secrethub-cli.rb
+++ Formula/secrethub-cli.rb
@@ -2,24 +2,24 @@
 class SecrethubCli < Formula
   desc "Command-line interface for SecretHub"
   homepage "https://secrethub.io"
-  version "0.42.1"
+  version "0.43.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/secrethub/secrethub-cli/releases/download/v0.42.1/secrethub-v0.42.1-darwin-amd64.tar.gz"
-    sha256 "0510f39c4b31a2c0b2ea282293642a38d82498458e3bb61715cc43af9028d075"
+    url "https://github.com/secrethub/secrethub-cli/releases/download/v0.43.0/secrethub-v0.43.0-darwin-amd64.tar.gz"
+    sha256 "c71c64084dbad45ad33a1a65d1f2606e542ea8fe5070741a3908dfa880f92be8"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/secrethub/secrethub-cli/releases/download/v0.42.1/secrethub-v0.42.1-linux-amd64.tar.gz"
-      sha256 "8a276e671fbf670764e3c64c860b7816ce458087a741aba177a6b25cae717dfb"
+      url "https://github.com/secrethub/secrethub-cli/releases/download/v0.43.0/secrethub-v0.43.0-linux-amd64.tar.gz"
+      sha256 "b9cf4b79c9220946d284eb87a77cc20f0470a80432b57df822568aed42aaf601"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/secrethub/secrethub-cli/releases/download/v0.42.1/secrethub-v0.42.1-linux-arm64.tar.gz"
-        sha256 "c8b13206cbe8ee0a5a75774e0e998afb2299741ba7c66e661d46ebacfdad0bcb"
+        url "https://github.com/secrethub/secrethub-cli/releases/download/v0.43.0/secrethub-v0.43.0-linux-arm64.tar.gz"
+        sha256 "7767a06f1e6cbbec7d31ae89543d9d8a17eb58e694b76c21b2e5f4b9b88049cd"
       else
-        url "https://github.com/secrethub/secrethub-cli/releases/download/v0.42.1/secrethub-v0.42.1-linux-armv6.tar.gz"
-        sha256 "b02038a969cd5b568bf388b46cc5dced1b75fa9c7f875d0ad202df7d476accc3"
+        url "https://github.com/secrethub/secrethub-cli/releases/download/v0.43.0/secrethub-v0.43.0-linux-armv6.tar.gz"
+        sha256 "92c474f90920e38011e3700419c6bbcd484b8d3f3cee398ee461c0c43ba976cd"
       end
     end
   end
